d462d2e1b3b366e0ec768e2f0323f8815c48804e
debshila <debshila@gmail.com>
2019-02-17 19:22:20 -0500

    Commenting out logger

--- flaskexample/__init__.py
+++ flaskexample/__init__.py
@@ -6,11 +6,11 @@ Created on Thu Jan 24 23:39:24 2019
 @author: dbm
 """
 from flask import Flask
-import logging
-import sys
+#import logging
+#import sys
 
 app = Flask(__name__, template_folder='template')
 from flaskexample import views
 
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
+#app.logger.addHandler(logging.StreamHandler(sys.stdout))
+#app.logger.setLevel(logging.ERROR)
